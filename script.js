c871d0fec231397f951ddd476bf14e296687eff0
UnUsuario-07 <vanesua1825@gmail.com>
2025-05-11 14:23:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  databaseURL: "https://TU_PROYECTO-default-rtdb.firebaseio.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "TU_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
 document.addEventListener('DOMContentLoaded', () => {
     const board = document.getElementById('board');
     const resetBtn = document.getElementById('reset');
